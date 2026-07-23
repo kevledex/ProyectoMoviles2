9458760f209794a1d808f141ae31b3a9010edf5f
kevincadena95 <kevgeek5@gmail.com>
2026-07-23 17:00:31 -0500

    Modificar la Targeta de Perfil

--- proyecto-moviles2/components/TarjetaPerfil.tsx
+++ proyecto-moviles2/components/TarjetaPerfil.tsx
@@ -1,54 +1,82 @@
-import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native'
+import {StyleSheet,Text,View,TouchableOpacity,Modal} from 'react-native'
 import React, { useState } from 'react'
 
-export default function TarjetaPerfil() {
+type Props = {
+    nick: string
+    correo: string
+    edad: number
+}
+
+export default function TarjetaPerfil({nick,correo, edad}: Props) {
+
     const [ocultarModal, setOcultarModal] = useState(false)
 
-    return (
-        <TouchableOpacity
-            onPress={() => setOcultarModal(true)}
-            style={styles.tarjeta}
-            activeOpacity={0.9}
-        >
-            <View style={styles.infoTarjeta}>
-                <Text style={styles.txtNombre}>Usuario</Text>
-                <Text style={styles.txtEmail}>correo@ejemplo.com</Text>
-                
-                <View style={styles.btnVerMas}>
-                    <Text style={styles.txtVerMas}>VER STATS</Text>
-                </View>
+return (
+    <TouchableOpacity
+        onPress={() => setOcultarModal(true)}
+        style={styles.tarjeta}
+        activeOpacity={0.9}
+    >
+        <View style={styles.infoTarjeta}>
+
+            <Text style={styles.txtNombre}>{nick}</Text>
+            <Text style={styles.txtEmail}>{correo}</Text>
+
+            <View style={styles.btnVerMas}>
+                <Text style={styles.txtVerMas}>
+                    VER STATS
+                </Text>
             </View>
 
-            <Modal
-                visible={ocultarModal}
-                transparent
-                animationType="fade"
-            >
-                <View style={styles.fondoModal}>
-                    <View style={styles.cuerpoModal}>
-                        
-                        <Text style={styles.tituloModal}>Usuario</Text>
-                        <Text style={styles.subtituloModal}>correo@ejemplo.com</Text>
+        </View>
 
-                        <View style={styles.cajaStats}>
-                            <View style={styles.filaStat}>
-                                <Text style={styles.txtStatLabel}>PARTIDAS:</Text>
-                                <Text style={styles.txtStatVal}>0</Text>
-                            </View>
+        <Modal
+            visible={ocultarModal}
+            transparent
+            animationType="fade"
+        >
+            <View style={styles.fondoModal}>
+                <View style={styles.cuerpoModal}>
+
+                    <Text style={styles.tituloModal}>{nick}</Text>
+                    <Text style={styles.subtituloModal}>{correo}</Text>
+
+                    <View style={styles.cajaStats}>
+
+                        <View style={styles.filaStat}>
+                             <Text style={styles.txtStatLabel}>
+                                EDAD:
+                            </Text>
+
+                            <Text style={styles.txtStatVal}>{edad}</Text>
                         </View>
 
-                        <TouchableOpacity
-                            style={styles.btnCerrar}
-                            onPress={() => setOcultarModal(false)}
-                        >
-                            <Text style={styles.txtBtnCerrar}>CERRAR</Text>
-                        </TouchableOpacity>
+                        <View style={styles.filaStat}>
+                            <Text style={styles.txtStatLabel}>
+                                PARTIDAS:
+                            </Text>
+
+                            <Text style={styles.txtStatVal}>
+                                0
+                            </Text>
+                        </View>
 
                     </View>
+
+                    <TouchableOpacity
+                        style={styles.btnCerrar}
+                        onPress={() => setOcultarModal(false)}
+                    >
+                        <Text style={styles.txtBtnCerrar}>
+                            CERRAR
+                        </Text>
+                    </TouchableOpacity>
+
                 </View>
-            </Modal>
-        </TouchableOpacity>
-    )
+            </View>
+         </Modal>
+    </TouchableOpacity>
+)
 }
 
 const styles = StyleSheet.create({
@@ -92,19 +120,19 @@ const styles = StyleSheet.create({
         fontSize: 16,
     },
     fondoModal: {
-        backgroundColor: "#000000d0",
+        backgroundColor: '#000000d0',
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     cuerpoModal: {
-        backgroundColor: "#121212",
-        width: "75%",
+        backgroundColor: '#121212',
+        width: '75%',
         borderRadius: 20,
         borderWidth: 3,
         borderColor: '#ffffff',
         padding: 20,
-        alignItems: "center",
+        alignItems: 'center',
     },
     tituloModal: {
         fontFamily: 'AmongUs',
@@ -152,5 +180,5 @@ const styles = StyleSheet.create({
         color: '#000000',
         fontFamily: 'AmongUs',
         fontSize: 18,
-    }
+    },
 })
